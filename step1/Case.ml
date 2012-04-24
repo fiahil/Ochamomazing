9f597860c498b0a12ab43f91227db5efb7c55ad3
benjamin businaro <busina_b@hall-of-busina.(none)>
2012-04-24 19:42:39 +0200

    patch case

--- step1/Case.ml
+++ step1/Case.ml
@@ -24,20 +24,20 @@ let color c =
 let set_color {color = c; sides = si} col =
   {color = col; sides = si}
 
-let statement {color = col ; sides = (n, s, e, w)} =
+let statement {color = col ; sides = (n, e, s, w)} =
   function
     | 0 -> n
-    | 1 -> s
-    | 2 -> e
+    | 1 -> e
+    | 2 -> s
     | 3 -> w
     | _ -> failwith "A case have only 4 sides"
 
-let set_side {color = col ; sides = (n, s, e, w)} elt_Type =
+let set_side {color = col ; sides = (n, e, s, w)} elt_Type =
   function
-    | 0 -> {color = col ; sides = (elt_Type, s, e, w)}
-    | 1 -> {color = col ; sides = (n, elt_Type, e, w)}
-    | 2 -> {color = col ; sides = (n, s, elt_Type, w)}
-    | 3 -> {color = col ; sides = (n, s, e, elt_Type)}
+    | 0 -> {color = col ; sides = (elt_Type, e, s, w)}
+    | 1 -> {color = col ; sides = (n, elt_Type, s, w)}
+    | 2 -> {color = col ; sides = (n, e, elt_Type, w)}
+    | 3 -> {color = col ; sides = (n, e, s, elt_Type)}
     | _ -> failwith "A case have only 4 sides"
 
 let create col =
