0d6dc9f67fba2c48eae016229d5b6ac1dd4aa2e9
Fiahil <fiahil@gmail.com>
2012-04-26 17:35:59 +0200

    modification du retour de set_color_at_pos

--- step1/Maze.mli
+++ step1/Maze.mli
@@ -11,4 +11,4 @@ type maze
 val create : int -> int -> maze
 val colorize : maze -> int -> int -> maze
 val get_case_at_pos : maze -> int * int -> Case.case
-val set_color_at_pos : maze -> int * int -> int -> maze
+val set_color_at_pos : maze -> int * int -> int -> int * int
