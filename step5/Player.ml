3d0d0083e28f3aff9aad5b5b7116e3915e4530cc
Fiahil <fiahil@gmail.com>
2012-04-29 20:26:20 +0200

    on the move

--- step5/Player.ml
+++ step5/Player.ml
@@ -21,38 +21,45 @@ module MakePlayer (Val : Maze.MAKEMAZE) : MAKEPLAYER
 struct
   type t = Val.maze
 
-  let player_pos = ref (0, 0)
+  let ppos = ref (0, 0)
   let player_dir = ref 0
 
   let init maze pos =
     begin
-      player_pos := pos;
+      ppos := pos;
       Val.set_color_at_pos maze pos 2
     end
 
-  let move maze =
-    let rec aux maze dir =
-      if dir >= Val.Elt.numberSides then
-	()
-      else
-	let case = Val.Elt.get_adj_case !player_pos (Val.Elt.get_dir_pattern dir)
+  let rec move maze =
+    if !player_dir >= Val.Elt.numberSides then
+      player_dir := 0
+    else
+      if Val.Elt.statement (Val.get_case_at_pos maze !ppos) !player_dir != Val.Elt.Wall
+      then
+	let case = Val.Elt.get_adj_case !ppos (Val.Elt.get_dir_pattern !player_dir)
 	in
 
-	if Val.get_color_at_pos maze case = 0 then
-	  aux maze (dir + 1)
+	if Val.get_color_at_pos maze case = 1 || Val.get_color_at_pos maze case = 3
+	then
+	  begin
+	    ignore (Val.set_color_at_pos maze !ppos 0);
+	    ppos := case;
+	    ignore (Val.set_color_at_pos maze !ppos 2)
+	  end
 	else
 	  begin
-	    ignore (Val.set_color_at_pos maze !player_pos 0);
-	    player_pos := case;
-	    ignore (Val.set_color_at_pos maze !player_pos 2)
+	    player_dir := !player_dir + 1;
+	    move maze
 	  end
-    in
-
-    aux maze !player_dir
+      else
+	begin
+	  player_dir := !player_dir + 1;
+	  move maze
+	end
 
   let dir () =
     !player_dir
 
   let pos () =
-    !player_pos
+    !ppos
 end
