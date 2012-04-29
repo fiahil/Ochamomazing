8d20c8435c149aad0afe0b15b349fd9d7e799ead
Fiahil <fiahil@gmail.com>
2012-04-29 20:41:56 +0200

    hop hop hop

--- step5/Player.ml
+++ step5/Player.ml
@@ -30,32 +30,39 @@ struct
       Val.set_color_at_pos maze pos 2
     end
 
-  let rec move maze =
-    if !player_dir >= Val.Elt.numberSides then
-      player_dir := 0
-    else
-      if Val.Elt.statement (Val.get_case_at_pos maze !ppos) !player_dir != Val.Elt.Wall
-      then
-	let case = Val.Elt.get_adj_case !ppos (Val.Elt.get_dir_pattern !player_dir)
-	in
+  let move maze =
+    begin
+      player_dir := 0;
+      let rec aux maze =
+	if !player_dir >= Val.Elt.numberSides then
+	  ()
+	else if Val.Elt.statement (Val.get_case_at_pos maze !ppos) !player_dir !=
+	       Val.Elt.Wall
+	  then
+	    let case = Val.Elt.get_adj_case !ppos (Val.Elt.get_dir_pattern !player_dir)
+	    in
 
-	if Val.get_color_at_pos maze case = 1 || Val.get_color_at_pos maze case = 3
-	then
-	  begin
-	    ignore (Val.set_color_at_pos maze !ppos 0);
-	    ppos := case;
-	    ignore (Val.set_color_at_pos maze !ppos 2)
-	  end
-	else
-	  begin
-	    player_dir := !player_dir + 1;
-	    move maze
-	  end
-      else
-	begin
-	  player_dir := !player_dir + 1;
-	  move maze
-	end
+	    if Val.get_color_at_pos maze case = 1 || Val.get_color_at_pos maze case = 3
+	    then
+	      begin
+		ignore (Val.set_color_at_pos maze !ppos 0);
+		ppos := case;
+		ignore (Val.set_color_at_pos maze !ppos 2)
+	      end
+	    else
+	      begin
+		player_dir := !player_dir + 1;
+		aux maze
+	      end
+	  else
+	    begin
+	      player_dir := !player_dir + 1;
+	      aux maze
+	    end
+      in
+
+      aux maze
+    end
 
   let dir () =
     !player_dir
